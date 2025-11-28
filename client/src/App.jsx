0f379f177eb2240808c046e2e8aa744317afe880
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-11-28 21:20:18 +0530

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Layout from './pages/Layout.jsx'
+import Preview from './pages/Preview.jsx'
+import ResumeBuild from './pages/ResumeBuild.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to the React Application!</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </>
   )
 }
 
