dfd5969264280b58d6c059abd1944ad14f2f20d4
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-16 17:09:22 +0530

    cursor pointer

--- client/src/Components/Home/Banner.jsx
+++ client/src/Components/Home/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div className="w-full py-3 font-semibold text-lg text-green-900 bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF] flex items-center justify-center">
+        <div className="w-full py-3 font-semibold text-sm text-yellow-900 bg-gradient-to-r from-[#F0E68C] to-[#FDFEFF] flex items-center justify-center">
             <p className="flex items-center justify-center">👦Bhaiya : Build your resume with AI</p>
         </div>
 )
