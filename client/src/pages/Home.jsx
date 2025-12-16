bcef2f53e0345ca4bfabb298b0e90b5406ff4c19
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-16 17:20:45 +0530

    featuue sedction

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../Components/Home/Banner'
 import Hero from '../Components/Home/Hero'
+import Features from '../Components/Home/Features'
 
 const Home = () => {
 return (
     <div>
         <Banner />
         <Hero />
+        <Features />
     </div>
     )
 }
