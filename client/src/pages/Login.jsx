1b634f1aa346e7a9b328b534e1434e0f3c270215
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-23 15:27:59 +0530

    login pagr

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,56 +1,55 @@
 import React from 'react'
 
 const Login = () => {
+  const [state, setState] = React.useState("login")
+
+    const [formData, setFormData] = React.useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prev => ({ ...prev, [name]: value }))
+    }
   return (
     <>
-    <div className='flex items-center justify-center mid-h-screen bg-grey-500'/>
-    <main className="flex items-center justify-center w-full px-4">
-            <form className="flex w-full flex-col max-w-96">
-        
-                <a href="https://prebuiltui.com" className="mb-8" title="Go to PrebuiltUI">
-                    <img src="../public/logo.svg" alt="logo" height="40" width="45"/>
-                </a>
-        
-                <h2 className="text-4xl font-medium text-gray-900">Sign in</h2>
-        
-                <p className="mt-4 text-base text-gray-500/90">
-                    Please enter email and password to access.
-                </p>
-        
-                <div className="mt-10">
-                    <label className="font-medium">Email</label>
-                    <input
-                        placeholder="Please enter your email"
-                        className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
-                        required
-                        type="email"
-                        name="email"
-                    />
+   <div className="flex items-center justify-center h-screen w-screen bg-gray-50">
+     <div className="flex justify-center">
+       <form onSubmit={handleSubmit} className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
+                <h1 className="text-gray-900 text-3xl mt-10 font-medium">{state === "login" ? "Login" : "Sign up"}</h1>
+                <p className="text-gray-500 text-sm mt-2">Please sign in to continue</p>
+                {state !== "login" && (
+                    <div className="flex items-center mt-6 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round-icon lucide-user-round"><circle cx="12" cy="8" r="5" /><path d="M20 21a8 8 0 0 0-16 0" /></svg>
+                        <input type="text" name="name" placeholder="Name" className="border-none outline-none ring-0" value={formData.name} onChange={handleChange} required />
+                    </div>
+                )}
+                <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect x="2" y="4" width="20" height="16" rx="2" /></svg>
+                    <input type="email" name="email" placeholder="Email id" className="border-none outline-none ring-0" value={formData.email} onChange={handleChange} required />
                 </div>
-        
-                <div className="mt-6">
-                    <label className="font-medium">Password</label>
-                    <input
-                        placeholder="Please enter your password"
-                        className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
-                        required
-                        type="password"
-                        name="password"
-                    />
+                <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-lock-icon lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2" /><path d="M7 11V7a5 5 0 0 1 10 0v4" /></svg>
+                    <input type="password" name="password" placeholder="Password" className="border-none outline-none ring-0" value={formData.password} onChange={handleChange} required />
                 </div>
-        
-                <button
-                    type="submit"
-                    className="mt-8 py-3 w-full cursor-pointer rounded-md bg-indigo-600 text-white transition hover:bg-indigo-700"
-                >
-                    Login
+                <div className="mt-4 text-left text-indigo-500">
+                    <button className="text-sm" type="reset">Forget password?</button>
+                </div>
+                <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
+                    {state === "login" ? "Login" : "Sign up"}
                 </button>
-                <p className='text-center py-8'>
-                    Don't have an account? <a href="/signup" className="text-indigo-600 hover:underline">Sign up</a>
-                </p>
+                <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-indigo-500 hover:underline">click here</a></p>
             </form>
-        </main>
-        </>
+        </div>
+    </div>
+    </>
   )
 }
 
