9e759b3b5e25d20e91e40fab56a005d1f261a36b
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-23 15:29:34 +0530

    login pagr

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                         <Link to='/app?state=register' className="hidden md:block px-6 py-2 bg-yellow-500 hover:bg-yellow-700 active:scale-95 transition-all rounded-full text-white">
                             Get started
                         </Link>
-                        <Link to='/app?state=login' className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
+                        <Link to='/app/login' className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
                             Login
                         </Link>
                     </div>
