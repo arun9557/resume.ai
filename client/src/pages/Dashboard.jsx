091519f115a36c98cf9302c59d49f5d2b97d5506
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-23 19:46:31 +0530

    dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from 'lucide-react'
+import { Plus, UploadCloud } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
@@ -8,11 +8,17 @@ const Dashboard = () => {
         <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-yellow-700 to-yellow-600 bg-clip-text text-transparent'>Hi, you are logged in</p>
 
         <div className='flex gap-4'>
-          <button>
-            <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradent-to-br hover:bg-yellow-600 text-white rounded-full'/>
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-600 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <Plus className='size-11 transition-all duration-300 p-2.5 bg-purple-600 text-white rounded-full'/>
+            <p>Create Resume</p>
           </button>
-
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-green-600 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <UploadCloud className='size-11 transition-all duration-300 p-2.5 bg-green-600 text-white rounded-full'/>
+            <p>Upload Existing Resume</p>
+          </button>
         </div>
+        
+        <hr className='border-slate-300 my-6 sm:w-[305px]'/>
       </div>
     </div>
   )
