71991a81a974cb558f13cd0995611e31e5a7f764
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-12-23 17:42:31 +0530

    button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
+import { PlusIcon } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Welcome to Dashboard Application!</h1>
+      <div className='max-w-7xl mx-auto px-4 py-8'>
+        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-yellow-700 to-yellow-600 bg-clip-text text-transparent'>Hi, you are logged in</p>
+
+        <div className='flex gap-4'>
+          <button>
+            <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradent-to-br hover:bg-yellow-600 text-white rounded-full'/>
+          </button>
+
+        </div>
+      </div>
     </div>
   )
 }
